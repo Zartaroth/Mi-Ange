1bab285c660660812ec0786b42d8bb1fd2a266e9
Zartaroth <jsalazarbenavente2014@gmail.com>
2024-05-13 23:18:16 -0500

    Agregar Diseño App

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const http = require('http');
-const PORT = 3000;
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('SE PUEDE CAMBIAR');
+  // Ruta al archivo index.html
+  const filePath = path.join(__dirname, 'index.html');
+
+  // Leer el contenido del archivo index.html
+  fs.readFile(filePath, 'utf8', (err, htmlContent) => {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error al leer el archivo HTML.');
+    }
+
+    // Configurar encabezados de respuesta
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    // Enviar el contenido del archivo HTML como respuesta
+    res.end(htmlContent);
+  });
 });
 
+// Puerto en el que se ejecutará el servidor
+const PORT = process.env.PORT || 3000;
+
+// Iniciar el servidor
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}/`);
 });
